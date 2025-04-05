ac0652f9a8ec709a338c225bb9d126124ecc7fb5
jaya <jaehyeon99@naver.com>
2025-04-05 22:56:44 +0900

    feat: 데이터셋 관리 및 JSON 로드를 위한 DataClass 추가
    - JSON 기반 데이터셋 로드를 처리하는 PyTorch `Dataset`인 `DataClass`를 구현했습니다.
    - 특정 인코딩으로 JSON 파일을 로드할 수 있는 `load_json` 함수 추가.
    - 타입 안정성과 가독성을 개선하기 위해 `Label` 및 `Data` 데이터 구조를 `@dataclass`로 구현.
    - 라벨 색상을 관리하기 위한 `ClassColor` 정의.
    - `torchvision.transforms`를 활용하여 커스텀 변환 파이프라인 구현.

---
+++ src/data.py
@@ -0,0 +1,93 @@
+import os
+import os.path as osp
+import json
+from dataclasses import dataclass
+import numpy as np
+from PIL import Image
+import torch
+from torch import Tensor
+from torch.utils.data import Dataset
+from torchvision import transforms as T
+
+
+class ClassColor:
+    t0: tuple = (255, 0, 0)
+    t1: tuple = (0, 255, 0)
+    t2: tuple = (0, 0, 255)
+    t3: tuple = (255, 255, 0)
+    t4: tuple = (255, 0, 255)
+
+
+@dataclass
+class Label:
+    sFileName: str
+    iLabel: int
+    iX: int
+    iY: int
+    iW: int
+    iH: int
+    tColor: tuple
+    sEncodingTest: str
+
+
+@dataclass
+class Data:
+    tImage: str
+    iLabel: int
+    iX: int
+    iY: int
+    iW: int
+    iH: int
+    tColor: tuple
+
+
+def load_json(
+        path: str,
+        encoding: str = 'cp949'
+) -> dict:
+    with open (path, 'r', encoding=encoding) as f:
+        dJsonDict = json.load(f)
+
+    return dJsonDict
+
+
+class DataClass(Dataset):
+    def __init__(self, sJsonBasePath: str):
+        self.m_lItemList = []
+        sJsonBasePath: str = sJsonBasePath
+        lJsonList = os.listdir(sJsonBasePath)
+
+        for sJson in lJsonList:
+            dItemDict = load_json(osp.join(sJsonBasePath, sJson))
+            self.m_lItemList.append(dItemDict)
+
+
+    def __getitem__(self, key):
+        dSelectItem = self.m_lItemList[key]
+        nImage: np.ndarray = np.array(Image.open(dSelectItem['sFilePath']).convert('RGB'))
+        tImage: Tensor = self.transform(nImage)
+
+        dResultDict = {}
+        dResultDict['tImage'] = tImage
+        dResultDict['nImage'] = nImage
+        dResultDict['iLabel'] = dSelectItem['lDataList']
+
+        return
+
+    def transform(self, nImage: np.ndarray) -> Tensor:
+        lTransformList = [
+            T.ToTensor()
+        ]
+
+        return torch.Compose(lTransformList)(nImage)
+
+
+
+
+
+if __name__ == '__main__':
+    _sInPath = rf'G:\data\test\label'
+
+    _DataClass = DataClass(sJsonBasePath=_sInPath)
+    a = 0
+
